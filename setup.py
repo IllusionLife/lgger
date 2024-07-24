9edb86c30b8c7e997fbc021a1fb4ca228ef5b19f
Marinos Papadimitriu <30587357+IllusionLife@users.noreply.github.com>
2024-07-24 19:35:27 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 # Fields marked as "Optional" may be commented out.
 setup(
     name="lgger",
-    version="0.1.3.5",
+    version="0.1.3.6",
     description="A package for writing logs to log files.",
     long_description=long_description,
     long_description_content_type="text/markdown",
